08dd3ea1bf4cf5b4f2097fe5cc53c3bd97494baa
Miller <miller_br@hotmail.com>
2018-03-16 01:27:28 -0300

    Calcula probabilidades condicionais das palavras

--- profiles/services.py
+++ profiles/services.py
@@ -1,4 +1,5 @@
 import unidecode
+import copy
 import xml.etree.ElementTree as ET
 from nltk.stem import RSLPStemmer
 from nltk.corpus import stopwords
@@ -39,36 +40,56 @@ class ServicesProfiles(object):
         Task.objects.create(label=label, task_type=task_type, process=instance)
 
     def recommend(self, instance):
-        priori_resource_types = {
-            'no_resource': 0
-        }
-        total_docs = 0
+
+        # calculate priori probability
+        priori_prob_by_resource_types = {}
         for resource in ResourceType.objects.all():
-            task_count = resource.task_set.all().count()
-            total_docs = total_docs + task_count
-            priori_resource_types[resource.name] = task_count
+            resource_task_count = resource.task_set.filter(process__organization=instance.organization).count()
+            priori_prob_by_resource_types[resource.name] = resource_task_count
 
-        for row in priori_resource_types:
-            priori_resource_types[row] = priori_resource_types[row] / task_count
+        task_count = Task.objects.filter(process__organization=instance.organization).count()
+        tasks_without_resource = Task.objects.filter(process__organization=instance.organization, application_type=None).count()
+        for row in priori_prob_by_resource_types:
+            priori_prob_by_resource_types[row] = priori_prob_by_resource_types[row] / task_count
+        priori_prob_by_resource_types['no_resources'] = tasks_without_resource/task_count
 
-        all_labels = {'undefined': []}  # dicionario com as labels de cada task por resource type
-        for process in instance.organization.process_set.all():
+
+        all_words_from_a_resource = {'undefined': []}
+        for process in instance.organization.process_set.all().exclude(id=instance.id):
             for task in process.task_set.all():
                 cleaned_label = self.clean_label(task.label)
                 application_name = task.application_type.name if task.application_type else 'undefined'
-                all_labels.setdefault(application_name, [])
-                all_labels[application_name] = all_labels[application_name] + cleaned_label
+                all_words_from_a_resource.setdefault(application_name, [])
+                all_words_from_a_resource[application_name] = all_words_from_a_resource[application_name] + cleaned_label
 
-        set_labels = {}
-        for i in all_labels:
+        set_of_unique_words_all_docs = set()
+        for i in all_words_from_a_resource:
             # cria set a partir das palavras de cada resource type
-            set_labels.setdefault(i, set())
-            set_labels[i] = set(all_labels[i])
+            set_of_unique_words_all_docs.update(all_words_from_a_resource[i])
 
-        print(all_labels)
-        print(set_labels)
-        # transformar lista em set
+        # adiciona as palavras do processo passado como instancia para o set
+        for task in instance.task_set.all():
+            set_of_unique_words_all_docs.update(self.clean_label(task.label))
+
         # calcular likelihood
+        word_counter = {}
+        for resource in all_words_from_a_resource:
+            word_counter[resource] = {}
+            for word in all_words_from_a_resource[resource]:
+                word_counter[resource][word] = all_words_from_a_resource[resource].count(word)
+
+        prob_condit = copy.deepcopy(word_counter)
+        for resource in prob_condit:
+            for word in set_of_unique_words_all_docs:
+                p_word = prob_condit[resource][word]+1 if word in prob_condit[resource] else 1
+                number_of_all_words_in_category = len(all_words_from_a_resource[resource])
+                number_unique_words_all_docs = len(set_of_unique_words_all_docs)
+                prob_condit[resource][word] = p_word/(number_of_all_words_in_category+number_unique_words_all_docs)
+
+
+        print(prob_condit)
+
+        # CALCULATE PROB
 
     def clean_label(self, label):
         '''
